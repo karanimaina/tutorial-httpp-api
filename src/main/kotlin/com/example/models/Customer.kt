f213588737239ae5e7a98976b14c21e592f10a62
felixmaina <karani.maina2010@gmail.com>
2022-06-23 01:24:12 +0300

    declare the get routing to get all the customers from the utable List

--- src/main/kotlin/com/example/models/Customer.kt
+++ src/main/kotlin/com/example/models/Customer.kt
@@ -1,10 +1,11 @@
 package com.example.models
 
+import kotlinx.serialization.Serializable
 
 
-@kotlinx.serialization.Serializable
-data class Customer(val id:String,val firstName:String,val lastName:String,val email:String,){
+@Serializable
+data class Customer(val id: String, val firstName: String, val lastName: String, val email: String)
+
+val customerStorage = mutableListOf<Customer>()
 
 
-
-}
