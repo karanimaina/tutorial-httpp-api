5da59e8fdb19e33856d2523f695bd9b4fe7d187f
felixmaina <karani.maina2010@gmail.com>
2022-06-23 15:29:45 +0300

    add the routes to the  appplication .kt  file

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,6 +1,9 @@
 package com.example.plugins
 
 import com.example.routes.customerRouting
+import com.example.routes.getOrderRoute
+import com.example.routes.listOrdersRoutes
+import com.example.routes.totalizeOrderRoute
 import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -10,5 +13,9 @@ import io.ktor.server.request.*
 fun Application.configureRouting() {
     routing {
         customerRouting()
+        listOrdersRoutes()
+        getOrderRoute()
+        totalizeOrderRoute()
+
     }
 }
--- src/main/kotlin/com/example/routes/ListOrdersRoutes.kt
+++ src/main/kotlin/com/example/routes/ListOrdersRoutes.kt
@@ -1,6 +1,7 @@
 package com.example.routes
 
 import com.example.models.orderStorage
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -10,6 +11,30 @@ fun Route.listOrdersRoutes() {
     if (orderStorage.isNotEmpty()){
         call.respond (orderStorage)
     }
+    }}
+    fun Route.getOrderRoute(){
+        get("/order/id?"){
+            val id = call.parameters["id"]?:return@get call.respondText ("bad request",status = HttpStatusCode.BadRequest)
+            val order = orderStorage.find{it.nummber==id}?:return@get call.respondText ( "Not Found", status = HttpStatusCode.NotFound)
+            call .respond(order)
+        }}
+
+    fun Route.totalizeOrderRoute() {
+        get("/order/{id?}/total") {
+            val id = call.parameters["id"] ?: return@get call.respondText("Bad Request", status = HttpStatusCode.BadRequest)
+            val order = orderStorage.find { it.nummber== id } ?: return@get call.respondText(
+                "Not Found",
+                status = HttpStatusCode.NotFound
+            )
+            val total = order.contents.sumOf { it.price * it.amount }
+            call.respond(total)
+        }
     }
 
-}
+
+
+
+
+
+
+
